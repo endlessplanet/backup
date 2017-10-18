196e4dc363a5aea377e1c7c3adb8d1946bb28a13
Emory Merryman <emory.merryman@gmail.com>
2017-10-18 03:29:42 +0000

    fix it

--- image/root/bin/backup-volumes.sh
+++ image/root/bin/backup-volumes.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-WORK=$(mktemp -d) &&
     docker volume ls --quiet | head -n 1 | while read VOLUME
     do
-        mkdir ${WORK}/${VOLUME} &&
+        WORK=$(mktemp -d) &&
+            mkdir ${WORK}/${VOLUME} &&
             docker inspect ${VOLUME} > ${WORK}/${VOLUME}/inspect.txt &&
             mkdir ${WORK}/${VOLUME}/content &&
             docker container run --interactive --rm --volume ${VOLUME}:/vol:ro --workdir /vol alpine:3.4 tar --create --file - . | tar --extract --file - --directory ${WORK}/${VOLUME}/content &&
             tar --create --file ${WORK}/${VOLUME}.tar --directory ${WORK}/${VOLUME} . &&
             gzip -9 ${WORK}/${VOLUME}.tar &&
             gpg --batch --yes --encrypt --armor --recipient ${1} --output ${WORK}/${VOLUME}.tar.gz.gpg ${WORK}/${VOLUME}.tar.gz &&
-            md5sum ${WORK}/${VOLUME}.tar.gz.gpg > ${WORK}/${VOLUME}.tar.gz.gpg.md5 &&
-            # aws s3 cp ${WORK}/${VOLUME}.tar.gz.gpg s3:// &&
-            true
+            md5sum ${WORK}/${VOLUME}.tar.gz.gpg | cut -b 1-32 > ${WORK}/${VOLUME}.tar.gz.gpg.md5 &&
+            aws s3 cp ${WORK}/${VOLUME}.tar.gz.gpg s3://${2}/${VOLUME}.tar.gz.gpg &&
+            aws s3 cp ${WORK}/${VOLUME}.tar.gz.gpg.md5 s3://${2}/${VOLUME}.tar.gz.gpg.md5 &&
+            rm -rf ${WORK}
     done
