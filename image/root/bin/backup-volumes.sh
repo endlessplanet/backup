2170f5d59f6077b7a1ed51cfb67edc4d1d9949be
Emory Merryman <emory.merryman@gmail.com>
2017-10-18 02:43:08 +0000

    wtf

--- image/root/bin/backup-volumes.sh
+++ image/root/bin/backup-volumes.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-docker volume ls --quiet | while read VOLUME
-do
-    docker inspect ${VOLUME}
-done
+WORK=$(mktemp -d) &&
+    docker volume ls --quiet | head -n 1 | while read VOLUME
+    do
+        mkdir ${WORK}/${VOLUME} &&
+            docker inspect ${VOLUME} > ${WORK}/${VOLUME}/inspect.txt &&
+            mkdir ${WORK}/${VOLUME}/content &&
+            docker container run --interactive --rm --volume ${VOLUME}:/vol:ro --workdir /vol alpine:3.4 tar --create --file - . | tar --extract --file - --directory ${WORK}/${VOLUME}/content &&
+            tar --create --file ${WORK}/${VOLUME}.tar --directory ${WORK}/${VOLUME} . &&
+            gzip -9 ${WORK}/${VOLUME}.tar
+    done
